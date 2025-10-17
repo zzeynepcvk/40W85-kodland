ac5018e22ad175faf65f74d33ed68e4551712b5b
Zeynep Çevik <95767424+zzeynepcvk@users.noreply.github.com>
2025-10-17 22:15:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-meme_dict = {
-            "CRINGE": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
-            }
+import discord
+from discord.ext import commands
 
-word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+description = """An example bot to showcase the discord.ext.commands extension
+module.
 
-if word in meme_dict.keys():
-    # Kelime eşleşiyorsa ne yapmalıyız?
-    print(meme_dict[word])
-else:
-    # Kelime eşleşmiyorsa ne yapmalıyız?
-    print("yok, şu kelimeler olabilir mi ? ")
+There are a number of utility commands being showcased here."""
 
-    
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    # Tell the type checker that User is filled up at this point
+    assert bot.user is not None
+
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    # Joined at can be None in very bizarre cases so just handle that as well
+    if member.joined_at is None:
+        await ctx.send(f'{member} has no join date.')
+    else:
+        await ctx.send(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+        
+
+
+
+
+
+bot.run("")
